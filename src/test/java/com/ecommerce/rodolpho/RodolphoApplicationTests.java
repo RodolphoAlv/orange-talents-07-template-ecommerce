ef4677af4c6e6b48b7ef446be0530d0333db396c
Rodolpho Alves <rodolpho.alves@zup.com.br>
2021-08-05 20:26:40 -0300

    setup

---
+++ src/test/java/com/ecommerce/rodolpho/RodolphoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.rodolpho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RodolphoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
